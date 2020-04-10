e42c4b2be0bf3b7a0ac2077d29df24b10091d322
Amjimenezv94 <amjimenezv@gmail.com>
2020-04-10 11:22:46 -0500

    branches_Pages

---
+++ src/pages/ResultTest.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ResultTest extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <h1> my home page</h1>  );
+    }
+}
+ 
+export default ResultTest;
---
+++ src/pages/userTest.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class UserTest extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <h1> my home page</h1>  );
+    }
+}
+ 
+export default UserTest;
