6ffb3ba4f6304ca9e566b23ffb0887b4399033a5
Amjimenezv94 <amjimenezv@gmail.com>
2020-04-11 10:39:58 -0500

    Solving folders uncomplete

---
+++ src/components/hola.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Hello extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <h1>Hola </h1> );
+    }
+}
+ 
+export default Hello;
