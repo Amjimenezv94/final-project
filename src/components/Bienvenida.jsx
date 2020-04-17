7083da987d70551bd659f7f1430daeab9c8be3bb
andreaclavijozuluaga <60928580+andreaclavijozuluaga@users.noreply.github.com>
2020-04-17 11:38:41 -0500

    json comienzos

--- src/components/Bienvenida.jsx
+++ src/components/Bienvenida.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../assets/CSS/bienvenida.css'
 
+
 class Bienvenida extends Component {
     constructor(props) {
         super(props);
@@ -20,7 +21,7 @@ class Bienvenida extends Component {
                     <div className="container">
                         <div className="row">
                             <div className="col-md-7">
-                                <input type="password" class="input-bienvenida form-control" id="inputP" placeholder="Escribenos tu nombre..."></input>
+                                <input type="text" class="input-bienvenida form-control" id="inputP" placeholder="Escribenos tu nombre..."></input>
                             </div>
                             <a className="boton-bienvenida btn btn-outline-light btn-lg mb-5" onClick={this.openTab}>ORIENTAME</a>
                         </div>
