82d970402f7839504de03cb23b3ec10001bea75d
Amjimenezv94 <amjimenezv@gmail.com>
2020-04-19 13:58:40 -0500

    explorar mejorado4

--- src/components/Bienvenida.jsx
+++ src/components/Bienvenida.jsx
@@ -16,7 +16,7 @@ class Bienvenida extends Component {
        // var local =JSON.stringify(localStorage.getItem("Nombre")
     openTab = () => {
         console.log('hello world');
-        document.getElementById('profesiones').click(); 
+        document.getElementById('pills-test-tab').click(); 
     };
 
     
