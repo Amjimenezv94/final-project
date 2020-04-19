272de4eb791f8414b03916702572fce0a8365f3f
Amjimenezv94 <amjimenezv@gmail.com>
2020-04-19 14:14:44 -0500

    userTest resuelto

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import HomePage from '../pages/homePage.jsx';
 import ResultTest from '../pages/ResultTest.jsx';
 import EduMod from '../pages/EduMod.jsx';
 import Contact from '../pages/Contact.jsx';
-import userTest from '../pages/userTest.jsx'
+import UserTest from '../pages/UserTest.jsx'
 import Explorar from './Explorar.jsx'
 
 
@@ -35,10 +35,10 @@ class NavBar extends Component {
                                 <a className="navegacion active text-light " id="pills-nosotros-tab" data-toggle="pill" href="#pills-nosotros" role="tab" aria-controls="pills-nosotros" aria-selected="true"> <p>Nosotros</p></a>
                             </li>
                             <li className="nav-item py-2 mt-2 mr-4" >
-                                <a className="navegacion text-light" id="pills-test-tab" data-toggle="pill" href="#pills-test" role="tab" aria-controls="pills-test" aria-selected="false"> <p>Orientame</p></a>
+                                <a className="navegacion text-light" id="pills-test-tab" data-toggle="pill" href="#pills-test" role="tab" aria-controls="pills-test" aria-selected="true"> <p>Orientame</p></a>
                             </li>
                             <li className="nav-item py-2 mt-2 mr-4" >
-                                <a className="navegacion text-light disabled" id="pills-profesiones-tab" data-toggle="pill" href="#pills-profesiones" role="tab" aria-controls="pills-profesiones" aria-selected="false" id="profesiones" > <p>Resultado</p></a>
+                                <a className="navegacion text-light disabled" id="pills-profesiones-tab" data-toggle="pill" href="#pills-profesiones" role="tab" aria-controls="pills-profesiones" aria-selected="true" id="profesiones" > <p>Resultado</p></a>
                             </li>
                             <li className="nav-item py-2 mt-2 mr-4" >
                                 <a className="navegacion   text-light" id="pills-explorar-tab" data-toggle="pill" href="#pills-explorar" role="tab" aria-controls="pills-explorar" aria-selected="true"> <p>Explorar</p></a>
@@ -57,7 +57,7 @@ class NavBar extends Component {
                         <div className="tab-content" id="pills-tabContent">
                             <div className="tab-pane fade show active " id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab"><HomePage /></div>
                             <div className="tab-pane fade " id="pills-nosotros" role="tabpanel" aria-labelledby="pills-nosotros-tab"><AcercaDeNosotros /></div>
-                            <div className="tab-pane fade" id="pills-test" role="tabpanel" aria-labelledby="pills-test-tab"><userTest/></div>
+                            <div className="tab-pane fade" id="pills-test" role="tabpanel" aria-labelledby="pills-test-tab"><UserTest/></div>
                             <div className="tab-pane fade" id="pills-profesiones" role="tabpanel" aria-labelledby="pills-profesiones-tab"><ResultTest /></div>
                             <div className="tab-pane fade" id="pills-explorar" role="tabpanel" aria-labelledby="pills-explorar-tab"><Explorar /></div>
                             <div className="tab-pane fade " id="pills-modalidad" role="tabpanel" aria-labelledby="pills-modalidad-tab"><EduMod /></div>
