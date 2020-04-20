03c94c98974dfc54f3a463b88630f58a22d5eeb1
Amjimenezv94 <amjimenezv@gmail.com>
2020-04-20 01:36:18 -0500

    contacto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -32,12 +32,12 @@ const Contacto = () => {
    
  
         return (
-                <div className="contenedr-contacto col-md-5 p-lg-4 my-5 ml-4 position-relative overflow-hidden p-1">
+                <div className="contenedor-contacto ">
                 
 		        { showMessage ? <div className="alert alert-success col-md-5 mx-auto" role="alert">Mensaje Enviado!!</div> : ``}
                 <div className="col-md-5 p-lg-4 my-5 ml-4">
-                    <h1 className="contacto font-weight-normal text-center"> CONTÁCTANOS </h1>
-                    <p className="parrafo-contacto lead font-weight-normal">
+                    <h1 className="contacto font-weight-normal text-center text-white"> CONTÁCTANOS </h1>
+                    <p className="parrafo-contacto lead font-weight-normal text-white px-0 ml-">
                         Nos encanta apoyarte en tu proceso, si tienes alguna duda o sugerencia puedes hacernos la llegar</p>
                    
                     
@@ -46,26 +46,26 @@ const Contacto = () => {
                             <div className="col-md-12">
                                 <div className="row">
                                     <div className=" col-md-6 mb-3">
-                                        <label for="firstName">Nombre</label>
+                                        <label for="firstName" className="text-white">Nombre</label>
                                         <input onChange={handleChange} name="firstName" value={contact.firstName} type="text" className="form-control" id="firstName" placeholder="Nombre/s" required="" />
                                         <div className="invalid-feedback">
                                             Valid first name is required.
                                         </div>
                                     </div>
                                     <div className="col-md-6 mb-3">
-                                        <label for="lastName">Apellido</label>
+                                        <label for="lastName" className="text-white">Apellido</label>
                                         <input  onChange={handleChange} name="lastName" value={contact.lastName} type="text" className="form-control" id="lastName" placeholder="Apellidos" required="" />
                                         <div className="invalid-feedback">
                                             Valid last name is required.
                                         </div>
                                     </div>
                                     <div className="col-md-12 mb-3">
-                                        <label for="email">Correo Electrónico</label>
+                                        <label for="email" className="text-white">Correo Electrónico</label>
                                         <input onChange={handleChange} name="email" value={contact.email}type="email" className="form-control" id="email" placeholder=" ejemplo@ejemplo.com"></input>
                                     </div>
                                    
                                     <div className="col-md-6 mb-3">
-                                            <label for="country">Pais</label>
+                                            <label for="country" className="text-white">Pais</label>
                                             <select className="custom-select d-block w-100" name="country" value={contact.country} onChange={handleChange} id="country" required="">
                                                 <option  value="" >Selecciona...</option>
                                                 <option>Colombia</option>
@@ -77,7 +77,7 @@ const Contacto = () => {
                                     </div>
 
                                     <div className="col-md-6 mb-3">
-                                            <label for="municipio">Municipio</label>
+                                            <label for="municipio" className="text-white">Municipio</label>
                                             <select onChange={handleChange} className="custom-select d-block w-100" name="municipio" value={contact.municipio} id="municipio" required="">
                                                 <option value="" >Selecciona...</option>
                                                 <option>Medellín</option>
@@ -95,7 +95,7 @@ const Contacto = () => {
                                     </div>
 
                                     <div className="col-md-12 mb-3">
-                                        <label for="comments">Comentarios</label>
+                                        <label for="comments" className="text-white">Comentarios</label>
                                         <input onChange={handleChange} name="comments" value={contact.comments}type="textarea" className="form-control" id="comments" placeholder="PQRS" required=""/>
                                         <div className="invalid-feedback">
                                             Please enter your comments.
