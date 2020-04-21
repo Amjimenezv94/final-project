5f2c4108bfc279aeb8c9def89dfee1a45baff22d
Amjimenezv94 <amjimenezv@gmail.com>
2020-04-21 09:31:08 -0500

    login

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 export default{
-  apiKey: "AIzaSyCzzoEGJopeuuiw7puMq06cJwGQlU1sCV0",
-  authDomain: "orientar-62559.firebaseapp.com",
-  databaseURL: "https://orientar-62559.firebaseio.com",
-  projectId: "orientar-62559",
-  storageBucket: "orientar-62559.appspot.com",
-  messagingSenderId: "595089117394",
-  appId: "1:595089117394:web:ca79076dbf399c4ddddd0b",
-  measurementId: "G-KHWLZ4M79M"
+  apiKey: "AIzaSyDvBGj6URWBPzqxE5qqg1ESrvEpu4YsnYQ",
+  authDomain: "orientame-f58af.firebaseapp.com",
+  databaseURL: "https://orientame-f58af.firebaseio.com",
+  projectId: "orientame-f58af",
+  storageBucket: "orientame-f58af.appspot.com",
+  messagingSenderId: "348379985657",
+  appId: "1:348379985657:web:7a2bb0cfde641afb3f1428",
+  measurementId: "G-QKLH1CJS2K"
 }
